c2691d81ae3183d147a59db2943c2e37af94085c
Ryan Safarzadeh <safarzadehryan@gmail.com>
2023-09-30 16:13:48 -0700

    play/pause setup

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-
-
-
-var canvas = document.createElement('canvas');
+var canvas = document.createElement("canvas");
 canvas.id = "canvas";
 document.body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
@@ -16,71 +13,96 @@ var originX = w / 2;
 var originY = h / 2;
 
 var Star = function () {
-    this.x = w * Math.random();
-    this.y = h * Math.random();
-    this.z = maxZ * Math.random();
-    this.accX = 0;
-    this.accY = 0;
-    this.c = Math.floor(Math.random() * 128) + 128;
-    this.color = "rgb(" + this.c + "," + this.c + "," + this.c + ")";
-}
+  this.x = w * Math.random();
+  this.y = h * Math.random();
+  this.z = maxZ * Math.random();
+  this.accX = 0;
+  this.accY = 0;
+  this.c = Math.floor(Math.random() * 128) + 128;
+  this.color = "rgb(" + this.c + "," + this.c + "," + this.c + ")";
+};
 
 Star.prototype.GetColorMagnitude = function () {
-    var m = 1 - (this.z / maxZ);
-    var r = Math.ceil(this.c * m);
-    return "rgb(" + r + "," + r + "," + r + ")";
-}
+  var m = 1 - this.z / maxZ;
+  var r = Math.ceil(this.c * m);
+  return "rgb(" + r + "," + r + "," + r + ")";
+};
 
 Star.prototype.Draw = function (ctx) {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, 2, 2);
-}
+  ctx.fillStyle = this.color;
+  ctx.fillRect(this.x, this.y, 2, 2);
+};
 
 Star.prototype.Update = function (ctx) {
-    if (this.x < 0 || this.x > w || this.y < 0 || this.y > h) {
-        this.z = maxZ;
-        this.accX = 0;
-        this.accY = 0;
-        this.x = w * Math.random();
-        this.y = h * Math.random();
-    }
-    var dx = (this.x - originX) / 1000;
-    var dy = (this.y - originY) / 1000;
-    this.accX += dx / dy;
-    this.accY += dy / dx;
-    this.x += dx;
-    this.y += dy;
-    this.z -= (Math.abs(dx) + Math.abs(dy));
-    if (this.z < 0)
-        this.z = 0;
-    this.color = this.GetColorMagnitude();
-}
+  if (this.x < 0 || this.x > w || this.y < 0 || this.y > h) {
+    this.z = maxZ;
+    this.accX = 0;
+    this.accY = 0;
+    this.x = w * Math.random();
+    this.y = h * Math.random();
+  }
+  var dx = (this.x - originX) / 1000;
+  var dy = (this.y - originY) / 1000;
+  this.accX += dx / dy;
+  this.accY += dy / dx;
+  this.x += dx;
+  this.y += dy;
+  this.z -= Math.abs(dx) + Math.abs(dy);
+  if (this.z < 0) this.z = 0;
+  this.color = this.GetColorMagnitude();
+};
 
 canvas.addEventListener("mousemove", function (e) {
-    originX = e.clientX;
-    originY = e.clientY;
+  originX = e.clientX;
+  originY = e.clientY;
 });
 
 function loop() {
-    w = window.innerWidth;
-    h = window.innerHeight;
-    canvas.width = w;
-    canvas.height = h;
+  w = window.innerWidth;
+  h = window.innerHeight;
+  canvas.width = w;
+  canvas.height = h;
 
-    ctx.clearRect(0, 0, w, h);
-    for (var i = 0; i < num_stars; i++) {
-        stars[i].Draw(ctx);
-        stars[i].Update();
-    }
-    requestAnimationFrame(loop);
+  ctx.clearRect(0, 0, w, h);
+  for (var i = 0; i < num_stars; i++) {
+    stars[i].Draw(ctx);
+    stars[i].Update();
+  }
+  requestAnimationFrame(loop);
 }
 
 //Create stars
-for (var i = 0; i < num_stars; i++)
-    stars.push(new Star());
+for (var i = 0; i < num_stars; i++) stars.push(new Star());
 loop();
 
-const sun = document.getElementById('toggleSun');
-sun.addEventListener('click', function() {
-    sun.classList.toggle('show-sun');
+const sun = document.getElementById("toggleSun");
+sun.addEventListener("click", function () {
+  sun.classList.toggle("show-sun");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const audio = document.getElementById("music");
+  const playBtn = document.getElementById("play-btn");
+  const playIcon = document.querySelector("#play-btn .fa-solid.fa-play");
+  const pauseIcon = document.querySelector("#play-btn .fa-solid.fa-pause");
+  let isPlaying = false;
+  pauseIcon.style.display = "none";
+
+  playBtn.addEventListener("click", () => {
+    if (isPlaying) {
+      audio.pause();
+      playIcon.style.display = "block";
+      pauseIcon.style.display = "none";
+    } else {
+      audio.play();
+      playIcon.style.display = "none";
+      pauseIcon.style.display = "block";
+    }
+    isPlaying = !isPlaying;
+  });
+});
+audio.addEventListener("ended", function () {
+  playIcon.style.display = "block";
+  pauseIcon.style.display = "none";
+  isPlaying = false;
 });
